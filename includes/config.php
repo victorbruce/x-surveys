48b3f16ce3e69989d72115672f5ef639b0b91c11
Samuel Boateng <kaybeta500@gmail.com>
2018-03-09 18:46:18 +0000

    working on db creds

--- includes/config.php
+++ includes/config.php
@@ -1,6 +1,8 @@
 <?php
-defined('DB_SERVER') ? NULL : define('DB_SERVER', 'localhost');
-defined('DB_USER')   ? NULL : define('DB_USER', 'xsurveyadmin');
-defined('DB_PASS')   ? NULL : define('DB_PASS', 'x-survey1234');
-defined('DB_NAME')   ? NULL : define('DB_NAME', 'xsurvey');
+$config = parse_ini_file('config.ini');
+
+defined('DB_SERVER') ? NULL : define('DB_SERVER', $config['host']);
+defined('DB_USER')   ? NULL : define('DB_USER', $config['username']);
+defined('DB_PASS')   ? NULL : define('DB_PASS', $config['password']);
+defined('DB_NAME')   ? NULL : define('DB_NAME', $config['db']);
 ?>
--- includes/database.php
+++ includes/database.php
@@ -1,5 +1,7 @@
 <?php
 
+include_once('config.php');
+
 class MySQLDatabase {
     private $connection;
     public $last_query;
