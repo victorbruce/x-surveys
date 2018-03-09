72623d80a22def949598eed61872ed7811d39932
Samuel Boateng <kaybeta500@gmail.com>
2018-03-09 21:10:43 +0000

    working on db  classes

--- public/index.php
+++ public/index.php
@@ -7,8 +7,31 @@
     <title>Document</title>
 </head>
 <body>
-    <a href="create_survey.php">Create Survey</a>
-    <a href="edit_survey.php">Edit Survey</a>
-    <a href="delete_survey.php">Delete Survey</a>
+
+    Create Survey
+    <form method="POST" action="create_survey.php">
+        Name: <input type="text" name="survey_name">
+        No_Questions: <input type="text" name="no_questions">
+        Category: <input type="text" name="category">
+        <input type="submit" value="Create" name="submit"/>
+    </form>
+
+    Edit Survey
+    <form method="POST" action="edit_survey.php">
+    Name:<input type="text" name="survey_name">
+    No_Questions:   <input type="text" name="no_questions">
+        <input type="text" name="category">
+        <input type="submit" value="Save" name="submit"/>
+    </form>
+
+    Delete Survey
+    <form method="POST" action="delete_survey.php">
+    Name: <input type="text" name="survey_name">
+    No_Questions    <input type="text" name="no_questions">
+        <input type="text" name="category">
+        <input type="submit" value="Delete" name="submit"/>
+    </form>
+
+
 </body>
 </html>
