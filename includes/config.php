ecc1b65dc584daf40aac0cbe7754c90cdc462346
Victor Bruce <victorbruce82@gmail.com>
2018-03-09 20:50:37 +0000

    Added database configurations

--- includes/config.php
+++ includes/config.php
@@ -1,5 +1,5 @@
 <?php
-defined('DB_SERVER') ? NULL : define('DB_SERVER', 'localhost');
+defined('DB_SERVER') ? NULL : define('DB_SERVER', '127.0.0.1');
 defined('DB_USER')   ? NULL : define('DB_USER', 'xsurveyadmin');
 defined('DB_PASS')   ? NULL : define('DB_PASS', 'x-survey1234');
 defined('DB_NAME')   ? NULL : define('DB_NAME', 'xsurvey');
--- includes/database.php
+++ includes/database.php
@@ -1,5 +1,7 @@
 <?php
 
+include ('database.php');
+
 class MySQLDatabase {
     private $connection;
     public $last_query;
@@ -23,42 +25,6 @@ class MySQLDatabase {
         }
     }
 
-    public function query($sql) {
-        $this->last_query = $sql;
-        $result = mysqli_query($this->connection, $sql);
-        $this->confirm_query($result);
-        return $result;
-    }
-
-    public function escape_value($value) {
-        $clean_value = mysqli_real_escape_string($this->connection, $value);
-        return $clean_value;
-    }
-
-    public function num_rows($result_set) {
-        return mysqli_num_rows($result_set);
-    }
-
-    public function insert_id() {
-        // get the last id inserted over the current db connection
-        return mysqli_insert_id($this->connection);
-    }
-
-    public function affected_rows() {
-        return mysqli_affected_rows($this->connection);
-    }
-
-    public function fetch_array($result_set) {
-        return mysqli_fetch_array($result_set);
-    }
-
-    private function confirm_query($result) {
-        if (!$result) {
-            $output  = "Database query failed: " .mysqli_connect_errno() ."<br><br>";
-            $output .= "Last query: " . $this->last_query . "<br>";
-            die($output);
-        }
-    }
 }
 $database = new MySQLDatabase();
 ?>
