4e6cdbb8d634fc71e3e5402fa5642a00df7d76b2
Victor Bruce <victorbruce82@gmail.com>
2018-03-09 16:41:44 +0000

    Project folder structure setup

---
+++ includes/config.php
@@ -0,0 +1,6 @@
+<?php
+defined('DB_SERVER') ? NULL : define('DB_SERVER', 'localhost');
+defined('DB_USER')   ? NULL : define('DB_USER', 'xsurveyadmin');
+defined('DB_PASS')   ? NULL : define('DB_PASS', 'x-survey1234');
+defined('DB_NAME')   ? NULL : define('DB_NAME', 'xsurvey');
+?>
